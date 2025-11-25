d388224065ad9cc27df3b90ba0bffb000ac044c7
Gabriel Witor <witor.alm@gmail.com>
2025-11-25 10:07:53 -0300

    feat: implement user filtering options in API and Postman collection

--- prodmanAPI/app/controllers/users_controller.rb
+++ prodmanAPI/app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    @users = build_users_query
     render json: { users: @users }, status: :ok
   end
 
@@ -68,5 +68,40 @@ class UsersController < ApplicationController
 
   def user_params
     params.permit(:name, :email, :password)
+  end
+
+  def build_users_query
+    # Start with base query - use unscoped if include_deleted is true
+    users = params[:include_deleted] == 'true' ? User.unscoped : User.all
+
+    # Filter by created_after date
+    if params[:created_after].present?
+      begin
+        date = Date.parse(params[:created_after])
+        users = users.where("created_at >= ?", date.beginning_of_day)
+      rescue ArgumentError
+        # Invalid date format, ignore parameter
+      end
+    end
+
+    # Filter by created_before date
+    if params[:created_before].present?
+      begin
+        date = Date.parse(params[:created_before])
+        users = users.where("created_at <= ?", date.end_of_day)
+      rescue ArgumentError
+        # Invalid date format, ignore parameter
+      end
+    end
+
+    # Filter by created_last_days (users created in the last X days)
+    if params[:created_last_days].present?
+      days = params[:created_last_days].to_i
+      if days > 0
+        users = users.where("created_at >= ?", days.days.ago)
+      end
+    end
+
+    users
   end
 end
